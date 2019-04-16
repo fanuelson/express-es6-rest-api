faec80ccb250057148b05f921b800a9035b860c7
ffnunes <fanuelson@gmail.com>
2019-04-16 09:00:11 -0300

    Removed and ignored dist directory.

--- dist/components/fornecedor/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var router = require("express").Router();
-
-var fornecedorController = require('./fornecedor.controller');
-
-router.use('/fornecedor', fornecedorController);
-module.exports = router;
--- dist/components/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var router = require("express").Router();
-
-var fornecedor = require('./fornecedor');
-
-router.use(fornecedor);
-module.exports = router;
